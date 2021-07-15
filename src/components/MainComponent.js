f1bceb9f39cf67f4ba03de5ffc4cc5c6fbfaed78
Hung K Nguyen <hungnguyenkhac7@gmail.com>
2021-07-15 22:37:56 +0700

    Create basic router to different pages

---
+++ src/components/MainComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Switch, Route, Redirect, withRouter } from "react-router-dom";
+import Home from './HomeComponent';
+import Login from './LoginComponent';
+import Signup from './SignupComponent';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Switch location={this.props.location}>
+                    {/* To be developed later */}
+                    <Route path='/home' component={() => <Home />} />
+                    {/* Hung 16/7 */}
+                    <Route path='/login' component={() => <Login />} />
+                    <Route path='/signup' component={() => <Signup />} />
+                    <Redirect to="/home" />
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Main
