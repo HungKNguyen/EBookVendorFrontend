17d0403de1f007792a742484ad1e712560cdb703
Hung K Nguyen <hungnguyenkhac7@gmail.com>
2021-07-19 07:43:25 +0700

    Added Review Post and read modal

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import {AppBar, Box, createTheme, ThemeProvider, Toolbar, Button} from "@material-ui/core";
+import {
+    AppBar,
+    Box,
+    createTheme,
+    ThemeProvider,
+    Toolbar,
+    Button,
+    Typography,
+    Modal,
+    TextField, Rating, Stack, Container
+} from "@material-ui/core";
 
 const theme = createTheme({
     palette: {
@@ -14,16 +24,81 @@ const theme = createTheme({
 });
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: 4,
+            review: "",
+            isOpen: false
+        }
+    }
+    handleOpen() {
+        this.setState({
+            isOpen: true
+        })
+    }
+    handleClose() {
+        this.setState({
+            isOpen: false
+        })
+    }
+    handleUserInput (e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value});
+    }
+    handleRating (newValue) {
+        this.setState({
+            rating: newValue
+        })
+    }
+    handleSubmit() {
+        console.log(this.state)
+        this.handleClose();
+    }
     render() {
+        const style = {
+            position: 'absolute',
+            top: '40%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 500,
+            background: '#fff',
+            border: '2px solid #272727',
+            p: 4,
+            textAlign: 'center'
+        };
         return (
             <ThemeProvider theme={theme}>
                 <Box sx={{ flexGrow: 1 }}>
                     <AppBar position="static" color='black'>
                         <Toolbar sx={{mx: 'auto'}}>
-                            <Button variant='text' size='large' color='white'>Write us a review</Button>
+                            <Button variant='text' size='large' color='white' onClick={() => this.handleOpen()}>Write us a review</Button>
                         </Toolbar>
                     </AppBar>
                 </Box>
+                <Modal
+                    open={this.state.isOpen}
+                    onClose={() => this.handleClose()}
+                >
+                    <Stack spacing={2} sx={style}>
+                        <Typography variant='h5'>Write a review</Typography>
+                        <TextField
+                            label="Multiline"
+                            multiline
+                            rows={4}
+                            fullWidth
+                            variant='filled'
+                            value={this.state.review}
+                            name='review'
+                            onChange={(event) => this.handleUserInput(event)}
+                        />
+                        <Stack direction='row' justifyContent="space-between">
+                            <Button variant="contained" onClick={() => this.handleSubmit()}>Submit</Button>
+                            <Rating size='large' value={this.state.rating} onChange={(event, newValue) => this.handleRating(newValue)}/>
+                        </Stack>
+                    </Stack>
+                </Modal>
             </ThemeProvider>
         )
     }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,15 +7,65 @@ import {
     Card,
     CardMedia,
     CardContent,
-    CardActionArea, Stack
+    CardActionArea, Stack, Rating, Modal, Divider
 } from "@material-ui/core";
 import Footer from "./FooterComponent";
 
+function ReviewModal(props) {
+    const style = {
+        position: 'absolute',
+        top: '40%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        background: '#fff',
+        border: '2px solid #272727',
+        p: 4,
+        textAlign: 'center'
+    };
+    if (props.content === null) return <div />
+    return (
+        <Modal
+            open={props.isOpen}
+            onClose={() => props.handleClose()}
+        >
+            <Stack sx={style} spacing={2}>
+                <Rating readOnly value={props.content.rating} />
+                <Stack direction='row' justifyContent='space-between'>
+                    <Typography variant='body2' sx={{textAlign: 'left'}}>{props.content.user.firstname + " " + props.content.user.lastname}</Typography>
+                    <Typography variant='body2' sx={{textAlign: 'left'}}>{props.content.date}</Typography>
+                </Stack>
+                <Divider />
+                <Typography variant='body1' sx={{textAlign: 'left'}}>{props.content.review}</Typography>
+            </Stack>
+        </Modal>
+    )
+}
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+            selectedReview: null
+        }
+    }
+    handleOpen(review) {
+        this.setState({
+            isOpen: true,
+            selectedReview: review
+        })
+    }
+    handleClose() {
+        this.setState({
+            isOpen: false,
+            selectedReview: null
+        })
+    }
     render() {
         const ebooks = this.props.ebooks.isLoading ? <div /> : this.props.ebooks.content.map((ebook) => {
             return (
-                <Grid item>
+                <Grid item key={ebook._id}>
                     <CardActionArea>
                         <Card sx={{ width: 200 }} variant="outlined">
                             <CardMedia
@@ -38,8 +88,8 @@ class Home extends Component {
         })
         const reviews = this.props.reviews.isLoading ? <div /> : this.props.reviews.content.map((review) => {
             return (
-                <div>
-                    <CardActionArea>
+                <div key={review._id}>
+                    <CardActionArea onClick={() => this.handleOpen(review)}>
                         <Card sx={{ width: 300}} variant="outlined">
                             <CardContent>
                                 <Typography gutterBottom variant="body1" component="div" className='three-line-para' align='left'>
@@ -57,6 +107,7 @@ class Home extends Component {
         return (
             <div>
                 <Header />
+                <ReviewModal content={this.state.selectedReview} isOpen={this.state.isOpen} handleClose={() => this.handleClose()}/>
                 <Box sx={{ mx: "auto", textAlign: 'center', py: 5}}>
                     <Typography variant='h4' component='div' sx={{ mb: 3 }}>
                         <b>Our Listing</b>
@@ -69,7 +120,7 @@ class Home extends Component {
                     <Typography variant='h4' component='div' sx={{ mb: 3 }}>
                         <b>Customers' Feedback</b>
                     </Typography>
-                    <Stack spacing={5} direction="row" justifyContent='center'>
+                    <Stack spacing={5} direction={{ xs: 'column', md: 'row' }} justifyContent='center' alignItems='center'>
                         {reviews}
                     </Stack>
                 </Box>
